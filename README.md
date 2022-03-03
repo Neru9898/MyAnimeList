c9b51e4f6cf569a8d89a8e23ae5a8dee8daa7777
Nerushan <nerushan@gojitech.co>
2022-03-03 14:33:59 -0500

    colour scheme and Left menu bar

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # MyAnimeList
+
+colour palate
+https://www.schemecolor.com/black-and-red-color-combination.php
+
+https://www.schemecolor.com/red-white-black.php
--- client/src/component/Home.tsx
+++ client/src/component/Home.tsx
@@ -1,21 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.scss";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
-import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import {
+  AppBar,
+  Drawer,
+  Toolbar,
+  Typography,
+  Box,
+  Grid,
+  List,
+  // Item,
+} from "@mui/material";
 import IconButton from "@mui/material/IconButton";
+// import
 const Home = () => {
+  const [open, isOpen] = useState<boolean>(false);
   return (
     <div className="home">
-      <AppBar style={{ background: "transparent", boxShadow: "none" }}>
+      <AppBar
+        className="navBar"
+        // style={{ background: "transparent", boxShadow: "none" }}
+      >
         <Toolbar>
-          <IconButton size="large" className="btn">
+          <IconButton
+            size="large"
+            className="btn"
+            onClick={() => isOpen(!open)}
+          >
             <MenuRoundedIcon fontSize="large" className="menuIcon" />
           </IconButton>
-          <Typography variant="h6" component="div">
+          <Typography variant="h6" component="div" className="text">
             MyAnimeList
           </Typography>
         </Toolbar>
       </AppBar>
+
+      <Drawer anchor="left" open={open} onClose={() => isOpen(false)}>
+        <Box className="menuContainer">
+          <Toolbar className="menuHeader">
+            <Typography variant="h5" component="div" className="text">
+              Menu
+            </Typography>
+            <IconButton
+              size="large"
+              // className="btn"
+              onClick={() => isOpen(!open)}
+            >
+              <CloseIcon fontSize="small" />
+            </IconButton>
+            {/* <CloseIcon></CloseIcon> */}
+          </Toolbar>
+          <List>time</List>
+          <List>time</List>
+          <List>time</List>
+          <List>time</List>
+          <List>time</List>
+          <List>time</List>
+          <List>time</List>
+          <List>time</List>
+        </Box>
+      </Drawer>
     </div>
   );
 };
