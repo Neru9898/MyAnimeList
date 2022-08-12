0c5159334be4892a744a165b57fcdfad396f78d2
Neru9898 <neonerushan@gmail.com>
2022-08-12 12:42:57 -0400

    Refactor Home Page

--- client/src/Component/TopAnimeContent.tsx
+++ client/src/Component/TopAnimeContent.tsx
@@ -17,24 +17,44 @@ const TopAnimeContent = () => {
     "https://api.jikan.moe/v4/top/anime?page=1&type=movie&filter=bypopularity";
   const topUpcomingMovieUrl =
     "https://api.jikan.moe/v4/top/anime?page=1&type=movie&filter=upcoming";
+  const topMangaUrl =
+    "https://api.jikan.moe/v4/top/manga?page=1&type=manga&filter=bypopularity";
+  const topManhwaUrl =
+    "https://api.jikan.moe/v4/top/manga?page=1&type=manhwa&filter=bypopularity";
+  const topManhuaUrl =
+    "https://api.jikan.moe/v4/top/manga?page=1&type=manhua&filter=bypopularity";
   const [animeList, setAnimeList] = useState<any>([]);
   const [movieList, setMovieList] = useState<any>([]);
   const [upcomingAnimeList, setUpcomingAnimeList] = useState<any>([]);
   const [upcomingMovieList, setUpcomingMovieList] = useState<any>([]);
+  const [mangaList, setMangaList] = useState<any>([]);
+  const [manhwaList, setManhwaList] = useState<any>([]);
+  const [manhuaList, setManhuaList] = useState<any>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<any>();
   const fetchData = async () => {
-    const topAnime = await axios.get(topAnimeUrl).then(await delay(1000));
-    const topMovie = await axios.get(topMovieUrl).then(await delay(2000));
+    const topAnime = await axios.get(topAnimeUrl).then(await delay(3000));
+    const topMovie = await axios.get(topMovieUrl).then(await delay(3000));
     const topUpcomingAnime = await axios
       .get(topUpcomingAnimeUrl)
       .then(await delay(3000));
     const topUpcomingMovie = await axios
       .get(topUpcomingMovieUrl)
-      .then(await delay(4000));
+      .then(await delay(6000));
+    const topManga = await axios.get(topMangaUrl).then(await delay(6000));
+    const topManhwa = await axios.get(topManhwaUrl).then(await delay(6000));
+    const topManhua = await axios.get(topManhuaUrl).then(await delay(9000));
     try {
       await axios
-        .all([topAnime, topMovie, topUpcomingAnime, topUpcomingMovie])
+        .all([
+          topAnime,
+          topMovie,
+          topUpcomingAnime,
+          topUpcomingMovie,
+          topManga,
+          topManhwa,
+          topManhua,
+        ])
         .then(
           axios.spread((...responses): any => {
             console.log(responses);
@@ -42,14 +62,14 @@ const TopAnimeContent = () => {
             setMovieList(responses[1].data.data);
             setUpcomingAnimeList(responses[2].data.data);
             setUpcomingMovieList(responses[3].data.data);
-
+            setMangaList(responses[4].data.data);
+            setManhwaList(responses[5].data.data);
+            setManhuaList(responses[6].data.data);
             setError("");
             setLoading(false);
           })
         );
     } catch (error) {
-      console.log(error);
-
       setError(error);
       setLoading(false);
     }
@@ -57,7 +77,6 @@ const TopAnimeContent = () => {
   useEffect(() => {
     // const topOva = axios.get(topOvaUrl);
     fetchData();
-    console.log("asldfj");
   }, []);
 
   return (
@@ -76,6 +95,9 @@ const TopAnimeContent = () => {
             displayList={upcomingMovieList}
             title={"Top Upcoming Movie"}
           />
+          <CarouselDisplay displayList={mangaList} title={"Top Magna"} />
+          <CarouselDisplay displayList={manhwaList} title={"Top Manhwa"} />
+          <CarouselDisplay displayList={manhuaList} title={"Top Manhua"} />
         </>
       )}
     </Box>
--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -2,16 +2,11 @@ import { Grid } from "@mui/material";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import "./Home.scss";
 import TopAnimeContent from "../Component/TopAnimeContent";
-import TopManga from "../Component/TopManga";
-import TopCharacters from "../Component/TopCharacters";
-import TopPeople from "../Component/TopPeople";
+
 const Home = () => {
   return (
     <Grid container className="home-container">
       <TopAnimeContent />
-      <TopManga />
-      <TopCharacters />
-      <TopPeople />
     </Grid>
   );
 };
