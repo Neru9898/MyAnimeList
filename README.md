47fda2402e6df501e3a43aac096f42efe5a1c08b
Neru9898 <neonerushan@gmail.com>
2022-10-25 20:53:05 -0400

    Created Nav bar

--- README.md
+++ README.md
@@ -10,10 +10,3 @@ https://www.schemecolor.com/red-white-black.php
 https://docs.api.jikan.moe/
 
 𝙈𝘼𝙇
-
-@font-face {
-font-family: 'Audiowide', cursive;
-/_a name to be used later_/
-src: url('https://fonts.googleapis.com/css2?family=Audiowide&display=swap');
-/_URL to font_/
-}
--- mal/src/App.tsx
+++ mal/src/App.tsx
@@ -3,7 +3,16 @@ import React from "react";
 import "./App.scss";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <div className="app-header">
+        <h1>MAL</h1>
+        <span>Anime</span>
+        <span>Manga</span>
+        <span>Movies</span>
+      </div>
+    </div>
+  );
 }
 
 export default App;
