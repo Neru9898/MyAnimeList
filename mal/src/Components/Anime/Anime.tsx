d502d9f44c89b0b99b5b171011370237c458cdcd
Neru9898 <neonerushan@gmail.com>
2022-10-26 14:25:09 -0400

    Set up anime page

--- mal/src/Components/Anime/Anime.tsx
+++ mal/src/Components/Anime/Anime.tsx
@@ -1,7 +1,31 @@
-import React from "react";
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "./Anime.scss";
 const Anime = () => {
-  return <div>Anime</div>;
+  const [animeList, setAnimeList] = useState<any>();
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    axios.get("https://api.jikan.moe/v4/top/anime").then((res: any) => {
+      setAnimeList(res.data.data);
+      setLoading(false);
+    });
+  }, []);
+
+  return (
+    <div className="anime-container">
+      {!loading &&
+        animeList.map((anime: any) => {
+          return (
+            <div className="grid-content">
+              <img src={anime.images.jpg.image_url} />
+              <span>{anime.title}</span>
+              <span>{anime.title_japanese}</span>
+            </div>
+          );
+        })}
+    </div>
+  );
 };
 
 export default Anime;
