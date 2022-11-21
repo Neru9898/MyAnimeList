ec7e835a0169e71802fecea1a446a768fc2f411f
Neru9898 <neonerushan@gmail.com>
2022-11-21 11:18:25 -0500

    Finsihed manga layout

--- mal/src/Components/Manga/Manga.tsx
+++ mal/src/Components/Manga/Manga.tsx
@@ -1,18 +1,20 @@
-import { CircularProgress, Pagination, Typography } from "@mui/material";
+import { CircularProgress, TextField, Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { delay } from "../../Helpers/delay";
 
 const Manga = () => {
   const [mangaList, setMangaList] = useState<any>();
   const [currManga, setCurrManga] = useState<any>();
+  const [currSearchManga, setCurrSearchManga] = useState<any>();
+  const [currSearch, setSearch] = useState<any>("");
   const [currPage, setCurrPage] = useState<any>({
     current: 1,
     last: 1,
   });
   const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
+  const getData = async () => {
     axios
       .get(
         `https://api.jikan.moe/v4/top/manga?type=manga&page=${currPage.current}`
@@ -24,34 +26,74 @@ const Manga = () => {
           last: res.data.pagination.last_visible_page,
         });
         setCurrManga(res.data.data[0]);
+      });
+
+    await delay(1000);
+  };
+  const getMangaSearch = async () => {
+    await delay(1000);
+
+    axios
+      .get(`https://api.jikan.moe/v4/manga?q=${currSearch}`)
+      .then((res: any) => {
+        setCurrSearchManga(res.data.data);
+
         setLoading(false);
       });
-  }, [currPage]);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  useEffect(() => {
+    getMangaSearch();
+  }, [currSearch]);
 
   const handleChange = (manga: any) => {
     setCurrManga(manga);
   };
 
-  const handlePageChange = (
-    event: React.ChangeEvent<unknown>,
-    value: number
-  ) => {
-    setCurrPage({ ...currPage, current: value });
-    setLoading(true);
-  };
+  // const handlePageChange = (
+  //   event: React.ChangeEvent<unknown>,
+  //   value: number
+  // ) => {
+  //   setCurrPage({ ...currPage, current: value });
+  //   setLoading(true);
+  // };
   return (
     <div className="anime-container">
       <div className="left-container">
         <div className="top-control-container">
-          <Typography variant="h5">Top Manga</Typography>{" "}
-          <Pagination
-            count={currPage.last}
-            page={currPage.current}
-            size="large"
+          <Typography variant="h5">Search Manga</Typography>{" "}
+          <TextField
+            id="outlined-basic"
+            label="Outlined"
             variant="outlined"
-            onChange={handlePageChange}
-            className="pagination"
+            onChange={(e) => setSearch(e.target.value)}
           />
+        </div>
+
+        {loading ? (
+          <CircularProgress />
+        ) : (
+          <div className="grid-container">
+            {currSearchManga.map((anime: any) => {
+              return (
+                <Link
+                  key={anime.mal_id}
+                  className="grid-content"
+                  to={`/manga/${anime.mal_id}`}
+                  onMouseOver={() => handleChange(anime)}
+                >
+                  <img src={anime.images.jpg.image_url} alt={anime.title} />
+                  <span>{anime.title}</span>
+                </Link>
+              );
+            })}
+          </div>
+        )}
+
+        <div className="top-control-container">
+          <Typography variant="h5">Top Manga</Typography>{" "}
         </div>
 
         {loading ? (
