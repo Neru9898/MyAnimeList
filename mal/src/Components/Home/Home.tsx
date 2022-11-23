fd62ccd71b4fa6a51a3a39d7df72f4e24bf0ee06
Neru9898 <neonerushan@gmail.com>
2022-11-23 12:56:12 -0500

    Home Page css styling

--- mal/src/Components/Home/Home.tsx
+++ mal/src/Components/Home/Home.tsx
@@ -1,6 +1,7 @@
 import { CircularProgress, Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { arrayToString } from "../../Helpers/arrayToString";
 
 import { delay } from "../../Helpers/delay";
 import "./Home.scss";
@@ -53,74 +54,87 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="home-container">
-      <div className="schedule-contatiner">
-        <Typography variant="h5">Today's Schedule</Typography>
+    <>
+      <Typography variant="h5">Welcome to MAL</Typography>
 
-        {loading ? (
-          <CircularProgress />
-        ) : (
-          schedule.map((anime: any) => {
-            return (
-              <div className="content">
-                <img src={anime.images.jpg.image_url} alt={anime.title} />
-                <div className="content-text">
-                  <span>{anime.title}</span>
-                  <span>Season: {anime.season}</span>
-                  <span>Day: {anime.broadcast.day}</span>
-                  <span>Time: {anime.broadcast.time}</span>
-                  <span>{anime.broadcast.string}</span>
-                  <span>Genre</span>
-                </div>
-              </div>
-            );
-          })
-        )}
-      </div>
+      <div className="home-container">
+        <div className="schedule-contatiner">
+          <Typography variant="h4">Today's Schedule</Typography>
 
-      <div className="right-conatiner">
-        <Typography variant="h5">Current Anime Season</Typography>{" "}
-        <div className="carousel-container">
           {loading ? (
             <CircularProgress />
           ) : (
-            currSeason.map((anime: any) => {
+            schedule.map((anime: any) => {
               return (
-                <div className="carousel-content">
-                  <img src={anime.images.jpg.image_url} alt="temp" />
-                  <div className="text-box">
-                    <Typography>{anime.title}</Typography>
-                    <Typography>Episodes: {anime.episodes}</Typography>
-                    <Typography>Ratings: {anime.rating}</Typography>
-                  </div>
-                </div>
-              );
-            })
-          )}
-        </div>
-        <Typography variant="h5">Next Anime Season</Typography>{" "}
-        <div className="carousel-container">
-          {loading ? (
-            <CircularProgress />
-          ) : (
-            nextSeason.map((anime: any) => {
-              return (
-                <div className="carousel-content">
-                  <img src={anime.images.jpg.image_url} alt="temp" />
-                  <div className="text-box">
+                <div className="content">
+                  <img src={anime.images.jpg.image_url} alt={anime.title} />
+                  <div className="content-text">
                     <span>{anime.title}</span>
+                    <span>Season: {anime.season}</span>
+                    <span>Day & Time: {anime.broadcast.string}</span>
                     <span>
-                      Episodes: {anime.episodes ? anime.episodes : "N/A"}
+                      Genre:{" "}
+                      {anime.genres.length !== 0
+                        ? arrayToString(anime.genres)
+                        : "N/A"}
                     </span>
-                    <span>Ratings: {anime.rating}</span>
                   </div>
                 </div>
               );
             })
           )}
         </div>
+        <div className="right-conatiner">
+          <Typography variant="h4">Current Anime Season</Typography>{" "}
+          <div className="carousel-container">
+            {loading ? (
+              <CircularProgress />
+            ) : (
+              currSeason.map((anime: any) => {
+                return (
+                  <div className="carousel-content">
+                    <img src={anime.images.jpg.image_url} alt="temp" />
+                    <div className="text-box">
+                      <Typography>{anime.title}</Typography>
+                      <Typography>Episodes: {anime.episodes}</Typography>
+                      <Typography>Ratings: {anime.rating}</Typography>
+                      <Typography>
+                        Genres:{" "}
+                        {anime.genres.length !== 0
+                          ? arrayToString(anime.genres)
+                          : "N/A"}
+                      </Typography>
+                      {/* <Typography>{anime.background}</Typography> */}
+                    </div>
+                  </div>
+                );
+              })
+            )}
+          </div>
+          <Typography variant="h4">Next Anime Season</Typography>{" "}
+          <div className="carousel-container">
+            {loading ? (
+              <CircularProgress />
+            ) : (
+              nextSeason.map((anime: any) => {
+                return (
+                  <div className="carousel-content">
+                    <img src={anime.images.jpg.image_url} alt="temp" />
+                    <div className="text-box">
+                      <span>{anime.title}</span>
+                      <span>
+                        Episodes: {anime.episodes ? anime.episodes : "N/A"}
+                      </span>
+                      <span>Ratings: {anime.rating}</span>
+                    </div>
+                  </div>
+                );
+              })
+            )}
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
